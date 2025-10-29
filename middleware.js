3fbfe03a24a1f5e3d18b0eb57223607cb0530b7d
황교준 <hwangkyojun7806@gmail.com>
2025-10-29 17:59:32 +0900

    chore : 일부러 시간 늦춰서 응답하는 코드 테스트

---
+++ middleware.js
@@ -0,0 +1,16 @@
+// middleware.js
+import { NextResponse } from "next/server";
+
+export async function middleware(request) {
+  // 1초 지연
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/dashboard/:path*", // 특정 페이지만
+    "/api/:path*", // API 라우트만
+    "/((?!_next/static|_next/image|favicon.ico).*)", // 정적 파일 제외
+  ],
+};

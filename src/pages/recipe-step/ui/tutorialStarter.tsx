c711bd5684e2f3880146846c5ba4cec1e989c7c0
황교준 <hwangkyojun7806@gmail.com>
2025-11-18 21:41:22 +0900

    fix : store 잘못 저장하는 로직 수정

--- src/pages/recipe-step/ui/tutorialStarter.tsx
+++ src/pages/recipe-step/ui/tutorialStarter.tsx
@@ -65,7 +65,7 @@ const useTutorialStarterStore = create<TimerStarter>()(
       },
     }),
     {
-      name: "step-tutorial-store",
+      name: "create-popover-store",
       storage: createJSONStorage(() =>
         typeof window !== "undefined" ? localStorage : noopStorage
       ),

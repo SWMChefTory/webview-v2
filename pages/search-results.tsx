9646b2cde00216247d63545d025d283b4d61496f
Changmin Han <49056953+hcmhcs@users.noreply.github.com>
2025-12-26 09:40:02 +0900

    refactor: Search Results 페이지 번역 시스템을 JSON 기반으로 마이그레이션

--- pages/search-results.tsx
+++ pages/search-results.tsx
@@ -8,7 +8,7 @@ export async function getStaticProps({ locale }: { locale: string }) {
     return {
       //props를 넘기면 useTranslation에서 가져올 수 있음.
       props: {
-        ...(await serverSideTranslations(locale, ["home","shared.recipe-creating-status-chip","common"], nextI18NextConfig)),
+        ...(await serverSideTranslations(locale, ["home","shared.recipe-creating-status-chip","common","search-results"], nextI18NextConfig)),
       },
     };
   }
